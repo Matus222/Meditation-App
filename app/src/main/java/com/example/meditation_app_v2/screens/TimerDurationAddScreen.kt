1bcf7b910161af31849d8ef49c36c033983c9d1b
Matúš Kendera <kendera10@stud.uniza.sk>
2024-06-16 22:48:51 +0200

    Mediation app (9.6.2024)

--- app/src/main/java/com/example/meditation_app_v2/screens/TimerDurationAddScreen.kt
+++ app/src/main/java/com/example/meditation_app_v2/screens/TimerDurationAddScreen.kt
@@ -19,11 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -210,5 +206,5 @@ fun SaveButton(
 @Preview(showBackground = true)
 fun TimerDurationAddScreenPreview() {
     val navController = rememberNavController()
-    //TimerDurationAddScreen(navController, TimerDurationEntryViewModel())
+    TimerDurationAddScreen(navController, TimerDurationEntryViewModel())
 }
